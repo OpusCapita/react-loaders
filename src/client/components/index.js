38af43b90289bd6c2039b000a1d597d32b88dc67
Kirill Volkovich <volkovich@scand.com>
2017-05-31 09:46:09 +0300

    Update gh-pages

--- src/client/components/index.js
+++
@@ -1,7 +0,0 @@
-import ScriptsLoaderContainer from './ScriptsLoaderContainer/ScriptsLoaderContainer.react';
-import serviceComponent from './serviceComponent/serviceComponent.react';
-
-export {
-  ScriptsLoaderContainer,
-  serviceComponent
-}
--- src/client/components/serviceComponent/index.js
+++
@@ -1,1 +0,0 @@
-export default require('./serviceComponent.react').default;
