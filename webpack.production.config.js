9429dfd2a5e9a72473577298ed1f20dd1bbe5fdf
Kirill Volkovich <volkovich@scand.com>
2017-06-08 13:06:29 +0300

    Fix webpack development config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,6 +6,7 @@ let config = require('./webpack.development.config.js');
 config.entry = path.resolve(path.join(__dirname, 'src', 'client', 'index.js'));
 delete config.devtool;
 delete config.output.publicPath;
+delete config.watch;
 config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
     'process.env': {
