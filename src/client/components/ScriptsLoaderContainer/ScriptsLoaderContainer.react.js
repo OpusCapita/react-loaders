9d75f4fabe9926aaa76279f8b017226ecdf80f28
Dmitriy Sanko <dsanko@scand.com>
2017-12-15 11:34:29 +0300

    Migrating to React 16.

--- src/client/components/ScriptsLoaderContainer/ScriptsLoaderContainer.react.js
+++ src/client/components/ScriptsLoaderContainer/ScriptsLoaderContainer.react.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ScriptsLoader from './scripts-loader.js';
 import isEqual from 'lodash/isEqual';
 
