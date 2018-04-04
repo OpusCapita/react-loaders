5cdb0e3e3d95bb1340e56054e0edca3f476a1916
Dmitry Shienok <dshienok@scand.com>
2018-04-04 16:35:40 +0300

    Updated release script. [ci skip]

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -174,9 +174,3 @@ echo "[INFO] ===================================================================
 git commit -am "Advancing project version to the next development - $SNAPSHOT_VERSION. [ci skip]"
 push
 
-echo -e "\n[INFO] ================================================================================================="
-echo "[INFO] Updating common dependencies."
-echo "[INFO] ================================================================================================="
-git clone git@github.com:OpusCapita/common-dependencies.git common-dependencies
-cd common-dependencies
-groovy /bin/commonDependenciesUpdate.groovy "${RELEASE_VERSION}"
