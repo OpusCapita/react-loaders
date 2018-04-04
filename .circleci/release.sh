edfb3f7eda6f7838facb503e7940113210600e68
Dmitry Shienok <dshienok@scand.com>
2018-04-04 16:33:16 +0300

    Updated release script. [ci skip]

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -31,7 +31,7 @@ node_version() {
     echo -e "\n[INFO] ================================================================================================="
     echo "[INFO] NodeJS: Updating to release version: $1."
     echo "[INFO] ================================================================================================="
-    find . -type f -name "package.json" -exec sed -i 's|\"version.*\"|\"version\": \"'$1'\"|g' {} \;
+    find . -maxdepth 1 -type f -name "package.json" -exec sed -i 's|\"version.*\"|\"version\": \"'$1'\"|g' {} \;
     git --no-pager diff --color
 }
 
