12d52b3600f7c9406a73771edf3005af2ea492e1
Dmitry Shienok <dshienok@scand.com>
2018-04-04 16:16:47 +0300

    Created new release branch. [ci skip]

--- src/server/demo/static/components/TextInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class TextInput extends React.Component {
-  render() {
-    return (
-      <input {...this.props} className="form-control" type="text"/>
-    )
-  }
-}
